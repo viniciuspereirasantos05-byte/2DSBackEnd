d759878dc3363797ba94ef6ed151034c2c14e4fa
𝖁𝖎𝖓𝖎𝖈𝖎𝖚𝖘 𝕾𝖆𝖓𝖙𝖙𝖘 <vinicius.pereira.santos05@escola.pr.gov.br>
2026-03-09 08:56:12 -0300

    Add files via upload
    aqui esta o codigo da primeira atividade

---
+++ aula1.js
@@ -0,0 +1,10 @@
+let nome = "Vinicius "
+let idade = 15
+let altura = 1.61
+let cor = "branco "
+let genero = "masculino"
+let cabelo = "curto, liso e loiro"
+let pcd = "apresento nehuma deficiencia,"
+let vestimenta = "usando uniforme escolar"
+
+console.log("fala rapaziadaaa, eu sou " + nome + "tenho " + idade + " anos, sou uma pessoa " + cor + " do genero " + genero + " tenho cabelo " +  cabelo + " não "+ pcd + "eu estou " + vestimenta)
