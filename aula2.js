8e2033518fd8310a1cf51016a989a43fc11c4ead
𝖁𝖎𝖓𝖎𝖈𝖎𝖚𝖘 𝕾𝖆𝖓𝖙𝖙𝖘 <vinicius.pereira.santos05@escola.pr.gov.br>
2026-03-11 09:39:33 -0300

    Add files via upload

---
+++ aula2.js
@@ -0,0 +1,11 @@
+const numero1 = 23
+const numero2 = 2
+const numero3 = 21
+let soma = numero1 + Number(numero3)
+let divisao = numero2 / numero1 
+let multiplicacao = numero1 * numero3 * numero2
+let subtracao = numero1 - numero3
+console.log("a subtração é " + subtracao)
+console.log("a divisão é " + divisao)
+console.log("a soma é " + soma )
+console.log("a multiplicão é " + multiplicacao)
