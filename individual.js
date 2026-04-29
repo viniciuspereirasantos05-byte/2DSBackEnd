025497c294b0816ca42311b18e24d97c8d82580f
𝖁𝖎𝖓𝖎𝖈𝖎𝖚𝖘 𝕾𝖆𝖓𝖙𝖙𝖘 <vinicius.pereira.santos05@escola.pr.gov.br>
2026-04-29 09:44:45 -0300

    Add files via upload

---
+++ individual.js
@@ -0,0 +1,26 @@
+const produtos = [
+    {
+        nome: "pão de queijo",
+        valor: "R$ 60,00 kg",
+        quantidade: 70,
+        peso: 150,
+        marca: "padaria Souza",
+        codigo: 21,
+    },
+    {
+        nome: "pão",
+        valor: "R$ 30,00",
+        quantidade: 200,
+        peso: 70,
+        marca: "padaria Souza",
+        codigo: 32,
+    },
+    {
+        nome: "pastel",
+        valor: "R$ 12,00",
+        quantidade: 30,
+        peso:  400,
+        marca: "padaria Souza",
+        codigo: 40,
+    },
+]
